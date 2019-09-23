4b37b00f57921ed2e137e7d57c224abca7f0b82e
Gabriel Neil <gabriel.neil@mercadolibre.com>
2019-09-23 15:57:46 -0300

    no message

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import csv
 import random
 
+# vars definition
 x0 = []
 x1 = []
 x2 = []
@@ -8,11 +9,14 @@ x3 = []
 x4 = []
 y = []
 w = []
+u = []
 
+# csv files reference
 with open('X_train.csv', 'r') as x_train, open('Y_train.csv', 'r') as y_train:
     csv_x_train = csv.reader(x_train)
     csv_y_train = csv.reader(y_train)
 
+    # loading x arrays
     for line in csv_x_train:
         x0.append(line[0])
         x1.append(line[1])
@@ -20,8 +24,15 @@ with open('X_train.csv', 'r') as x_train, open('Y_train.csv', 'r') as y_train:
         x3.append(line[3])
         x4.append(line[4])
 
+        # loading y array
         for line in csv_y_train:
             y.append(line)
 
+# init randoms weight
 for x in range(5):
     w.append(random.random())
+
+# calculation
+for x in range(x0.__len__()):
+    u.append((float(x0[x]) * float(w[0])) + (float(x1[x]) * float(w[1])) + (float(x2[x]) * float(w[2])) + (
+            float(x3[x]) * float(w[3])) + (float(x4[x]) * float(w[4])))
