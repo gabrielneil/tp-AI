906d42e3fb5b6a240750a07aec96c77125e251d9
Gabriel Neil <gabriel.neil@mercadolibre.com>
2019-09-23 17:58:16 -0300

    no message

--- Main.py
+++ Main.py
@@ -11,13 +11,15 @@ x4 = []
 y = []
 w = []
 u = []
-error = []
+errors = []
+deltaW = []
+
 # csv files reference
 with open('X_train.csv', 'r') as x_train, open('Y_train.csv', 'r') as y_train:
     csv_x_train = csv.reader(x_train)
     csv_y_train = csv.reader(y_train)
 
-    # loading x arrays
+    # loading X arrays
     for line in csv_x_train:
         x0.append(line[0])
         x1.append(line[1])
@@ -25,21 +27,24 @@ with open('X_train.csv', 'r') as x_train, open('Y_train.csv', 'r') as y_train:
         x3.append(line[3])
         x4.append(line[4])
 
-        # loading y array
+        # loading Y array
         yAux = []
     for line in csv_y_train:
         yAux.append(line)
 
 y = np.array(yAux)
 
-# init randoms weight
+# init random weights
 for x in range(5):
     w.append(random.random())
 
-# calculation
+# calculate
 for x in range(x0.__len__()):
     oK = (float(x0[x]) * float(w[0])) + (float(x1[x]) * float(w[1])) + (float(x2[x]) * float(w[2])) + (
             float(x3[x]) * float(w[3])) + (float(x4[x]) * float(w[4]))
     u.append(oK)
-    result = float(y[x]) - oK
-    error.append(result)
+    error = float(y[x]) - oK
+    errors.append(error)
+    preDeltaW = (0.2 * error)
+
+print(errors)
