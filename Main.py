e348dc156efee77d7ba2559051874d6537f5cf8d
Gabriel Neil <gabriel.neil@mercadolibre.com>
2019-09-23 13:43:00 -0300

    no message

--- Main.py
+++ Main.py
@@ -1,24 +1,27 @@
 import csv
 import random
 
-with open('X_train.csv', 'r') as x_train:
+x0 = []
+x1 = []
+x2 = []
+x3 = []
+x4 = []
+y = []
+w = []
+
+with open('X_train.csv', 'r') as x_train, open('Y_train.csv', 'r') as y_train:
     csv_x_train = csv.reader(x_train)
-
-    for line in csv_x_train :
-        x0 = line[0]
-        x1 = line[1]
-        x2 = line[2]
-        x3 = line[3]
-        x4 = line[4]
-
-with open('Y_train.csv', 'r') as y_train:
     csv_y_train = csv.reader(y_train)
-    for line in csv_y_train:
-        y0 = line
+
+    for line in csv_x_train:
+        x0.append(line[0])
+        x1.append(line[1])
+        x2.append(line[2])
+        x3.append(line[3])
+        x4.append(line[4])
+
+        for line in csv_y_train:
+            y.append(line)
 
 for x in range(5):
-    w0 = random.random()
-    w1 = random.random()
-    w2 = random.random()
-    w3 = random.random()
-    w4 = random.random()
+    w.append(random.random())
